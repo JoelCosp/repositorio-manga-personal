3adae605f79b68806399d490dc22582cffd9e749
JoelVRV <145133218+JoelDAW2@users.noreply.github.com>
2025-01-12 18:29:27 +0100

    UPDATE BBDD
    - El proyecto esta igual solo que en vez de guardar un video, guarda informacion sobre mangas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,12 +17,12 @@ import { addDoc } from 'firebase/firestore'; // Para agregar documentos a Firest
 export class AppComponent {
   title = 'ff_project_1'; // Variable para almacenar el titulo de la aplicacion
   items$: Observable<any[]>; // Observable que contiene la lista de videos de FIRESTORE
-  newItem = { titulo: '', url: '' }; // Objeto para almacenar los datos del nuevo video que se va a agregar
+  newItem = { title: '', description: '', gender: '', author: '', publication_date: '', state: '', img_url: '', qualification: '' }; // Objeto para almacenar los datos del nuevo video que se va a agregar
 
   // El constructor es donde se inicializan los servicios y datos del componente
   constructor() {
     const firestore = inject(Firestore); // Usamos 'inject' para inyectar Firestore, el servicio de base de datos de Firebase
-    const itemsCollection = collection(firestore, 'videos'); // Referenciamos la colección 'videos' en Firestore
+    const itemsCollection = collection(firestore, 'Mangas'); // Referenciamos la colección 'videos' en Firestore
 
     this.items$ = collectionData(itemsCollection); // Asignamos a 'items$' los datos de la colección 'videos' como un flujo de datos (Observable)
 
@@ -31,14 +31,20 @@ export class AppComponent {
 
   // Método asíncrono para agregar datos de ejemplo a la colección 'videos' si está vacía
   async addExampleData(firestore:Firestore) {
-    const itemsCollection = collection(firestore, 'videos'); // Referenciamos la colección 'videos' de Firestore nuevamente
+    const itemsCollection = collection(firestore, 'Mangas'); // Referenciamos la colección 'videos' de Firestore nuevamente
     const snapshot = await getDocs(itemsCollection); // Obtenemos los documentos de la colección
 
     // Si la colección está vacía, agregamos un video de ejemplo
     if(snapshot.empty) {
       const exampleItem =  {
-        titulo: 'Video 1',
-        url: 'https://www.youtube.com/watch?v=kVs1vVTLtiE',
+        title: 'One piece', 
+        description: 'Una tripulación de piratas zarpa a la mar en busca del One Piece', 
+        gender: 'Shonen', 
+        author: 'Eiichirō Oda', 
+        publication_date: '22/07/1997', 
+        state: 'Activo', 
+        img_url: 'https://pm1.aminoapps.com/8027/bf7d0192f89db68b71b72a718c2a14ebe8f158dar1-1024-1603v2_uhq.jpg', 
+        qualification: '4.8'
       }
       try {
         // Agregamos el video de ejemplo a Firestore
